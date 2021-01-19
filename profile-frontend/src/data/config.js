ff0fd72f6fdb5ddb4a35fe399ee7507b9ab3153b
Jaynil1611 <48921037+Jaynil1611@users.noreply.github.com>
2021-01-19 16:20:00 +0530

    first commit

---
+++ profile-frontend/src/data/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // TODO: change your profile information here
+  name: "Jaynil Gaglani",
+  greeting: "Hello 👋🏻",
+  greetingDescription:
+    "I'm Jaynil Gaglani a, Software Engineer, Fullstack developer & ML aspirant!",
+  githubUrl: "https://github.com/Jaynil1611",
+  linkedinUrl: "https://www.linkedin.com/in/jaynilgaglani/",
+  cvLink: "https://jaynil1611.github.io/Resume/",
+};
